76939bba4ef64661b06e7b5a492ffab49c4cf50e
Erik Kaufman <141158064+Erik-Kaufman@users.noreply.github.com>
2024-11-09 21:53:07 -0600

    Fixed some of the unit converstions

--- process_stars.py
+++ process_stars.py
@@ -13,39 +13,21 @@ import math
 
 # get data from the json file
 #
-# Declaenation is the horizontal, (DEC)
-# Right Ascension is the veritcal (RA)
+# Declaenation is the x, (DEC)
+# Right Ascension is the y (RA)
 #
-
-vertical_size = 6000
-horizontal_size = 8000
-
-# will return an intiger that will hold the position of the star as an int
-def convert_RA_to_num(angle):
-    # 1/24 of a circle per hour
-    # 1/1440 of a circle per minuet
-    # 1/86400 of a circle per second
-    # half of a circle is 12 * 3725 = 
-
-    # in the format of "+00:00:00.00"
-
-    pixle = 0
-    pixle += int(angle[0:2]) * 3725 # hour
-    pixle += int(angle[3:5]) * 60 # minuet
-    pixle += int(angle[6:8]) # seconds
-
-
-    return pixle
+x_size = 1000
+y_size = 600
 
 def convert_DEC_to_num(angle):
     # degrees minuets seconds
     # half is 90 degrees becasue plus or minus
-
+    # full range is 3600 * 180 = 648000
 
     # in the format of "+00:00:00.00"
 
     pixle = 0
-    pixle += int(angle[1:3]) * 360 # degrees
+    pixle += int(angle[1:3]) * 3600 # degrees
     pixle += int(angle[4:6]) * 60 # minuet
     pixle += int(angle[7:9]) # seconds
 
@@ -56,36 +38,50 @@ def convert_DEC_to_num(angle):
 
     return pixle
 
+# will return an intiger that will hold the position of the star as an int
+def convert_RA_to_num(angle):
+    # 1/24 of a circle per hour
+    # 1/1440 of a circle per minuet
+    # 1/86400 of a circle per second
+    # total range is 24 * 3600 = 93600
+
+    # in the format of "00:00:00.00"
+
+    pixle = 0
+    pixle += int(angle[0:2]) * 3600 # hour
+    pixle += int(angle[3:5]) * 60 # minuet
+    pixle += int(angle[6:8]) # seconds
+
+
+    return pixle
 
 with open("BSC.json") as file:
     # create the image
-    im = PIL.Image.new(mode="RGB", size=(horizontal_size, vertical_size))
+    im = PIL.Image.new(mode="RGB", size=(x_size, y_size))
     
     pixles = im.load()
 
     for x in range(im.size[0]):
-        pixles[x, vertical_size/2] = (255, 255, 255)
+        pixles[x, y_size/2] = (255, 255, 255)
     for y in range(im.size[1]):
-        pixles[horizontal_size/2, y] = (255, 255, 255)
+        pixles[x_size/2, y] = (255, 255, 255)
 
     # loop through the data and update a pixle for each star
-    for item in json.load(file):
+    for i, item in enumerate(json.load(file)):
         # convert the hours, degrees, minuts, seconds into a float, then scale it to the image and plot it
-        ra_num = convert_RA_to_num(item["RA"])
         dec_num = convert_DEC_to_num(item["DEC"])
-
-        print(ra_num)
-        print(dec_num)
+        ra_num = convert_RA_to_num(item["RA"])
 
         # remap the pizles by dividing by the range of the pizles, then by multiplying by the screen size
-        ra_pixle = math.floor((ra_num / 89400) * vertical_size)
-        dec_pixle = math.floor((dec_num / 89400) * horizontal_size)
-        print(ra_pixle)
-        print(dec_pixle)
+        dec_pixle = math.floor((dec_num / 648000) * x_size)
+        ra_pixle = math.floor((ra_num / 93600) * y_size)
+        print(f"{i} x: {dec_pixle}")
+        print(f"{i} y: {ra_pixle}")
+        print(item)
 
         # set the pixles
-        pixles[ra_pixle, dec_pixle] = (255, 255, 255)
+        pixles[dec_pixle, ra_pixle] = (255, 255, 255)
 
 
-    im.show()
+im.show()
 
