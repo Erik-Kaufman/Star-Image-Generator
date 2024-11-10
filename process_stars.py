8e9fb47af971c87182741694c7fce865d807e4d1
Erik Kaufman <141158064+Erik-Kaufman@users.noreply.github.com>
2024-11-09 23:19:27 -0600

    the stars are now in a circle

--- process_stars.py
+++ process_stars.py
@@ -10,36 +10,44 @@ import json
 import PIL.Image
 import math
 
+# try converting to spherical cordinates to see it it is what I want
+
+# from https://en.wikipedia.org/wiki/Equatorial_coordinate_system
+# z sin del
+# x cos del * cos ra
+# y cos del * sin ra
+
 
 # get data from the json file
 #
 # Declaenation is the x, (DEC)
 # Right Ascension is the y (RA)
 #
-x_size = 1000
-y_size = 600
+x_size = 2000
+y_size = 2000
 
-def convert_DEC_to_num(angle):
+def convert_DEC_to_radians(angle):
     # degrees minuets seconds
     # half is 90 degrees becasue plus or minus
     # full range is 3600 * 180 = 648000
 
     # in the format of "+00:00:00.00"
 
-    pixle = 0
-    pixle += int(angle[1:3]) * 3600 # degrees
-    pixle += int(angle[4:6]) * 60 # minuet
-    pixle += int(angle[7:9]) # seconds
+    deg = 0.0
+    deg += float(angle[1:3])  # degrees
+    deg += float(angle[4:6]) / 60 # minuet
+    deg += float(angle[7:12]) / 3600 # seconds
 
     if angle[0] == "-":
-        pixle = 32400 - pixle
-    else:
-        pixle = 32400 + pixle
+        deg = 90.0 - deg
+    else: 
+        deg = 90.0 + deg
 
-    return pixle
+    radians = (deg / 360.0) * (math.pi * 2)
+    return radians
 
 # will return an intiger that will hold the position of the star as an int
-def convert_RA_to_num(angle):
+def convert_RA_to_radians(angle):
     # 1/24 of a circle per hour
     # 1/1440 of a circle per minuet
     # 1/86400 of a circle per second
@@ -47,13 +55,13 @@ def convert_RA_to_num(angle):
 
     # in the format of "00:00:00.00"
 
-    pixle = 0
-    pixle += int(angle[0:2]) * 3600 # hour
-    pixle += int(angle[3:5]) * 60 # minuet
-    pixle += int(angle[6:8]) # seconds
+    circle = 0
+    circle += float(angle[0:2]) / 24.0 # hour
+    circle += float(angle[3:5]) / 1440 # minuet
+    circle += float(angle[6:11]) / 86400# seconds
 
-
-    return pixle
+    # multiply by two pi to get to radians
+    return circle * 2 * math.pi
 
 with open("BSC.json") as file:
     # create the image
@@ -61,26 +69,31 @@ with open("BSC.json") as file:
     
     pixles = im.load()
 
-    for x in range(im.size[0]):
-        pixles[x, y_size/2] = (255, 255, 255)
-    for y in range(im.size[1]):
-        pixles[x_size/2, y] = (255, 255, 255)
+    #for x in range(im.size[0]):
+    #    pixles[x, y_size/2] = (255, 255, 255)
+    #for y in range(im.size[1]):
+    #    pixles[x_size/2, y] = (255, 255, 255)
 
     # loop through the data and update a pixle for each star
     for i, item in enumerate(json.load(file)):
         # convert the hours, degrees, minuts, seconds into a float, then scale it to the image and plot it
-        dec_num = convert_DEC_to_num(item["DEC"])
-        ra_num = convert_RA_to_num(item["RA"])
+        dec_rad = convert_DEC_to_radians(item["DEC"])
+        ra_rad = convert_RA_to_radians(item["RA"])
 
         # remap the pizles by dividing by the range of the pizles, then by multiplying by the screen size
-        dec_pixle = math.floor((dec_num / 648000) * x_size)
-        ra_pixle = math.floor((ra_num / 93600) * y_size)
-        print(f"{i} x: {dec_pixle}")
-        print(f"{i} y: {ra_pixle}")
+        x = math.cos(dec_rad) * math.cos(ra_rad) * x_size / 2.0
+        y = math.cos(dec_rad) * math.sin(ra_rad) * y_size / 2.0
+
+        # scale it back to positive numbers
+        x += x_size / 2
+        y += y_size / 2
+
+        print(f"{i} x: {math.floor(x)}")
+        print(f"{i} y: {math.floor(y)}")
         print(item)
 
         # set the pixles
-        pixles[dec_pixle, ra_pixle] = (255, 255, 255)
+        pixles[math.floor(x), math.floor(y)] = (255, 255, 255)
 
 
 im.show()
